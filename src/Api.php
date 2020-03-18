5baed2e56c49ba7bfdc43cb8e534b814946aebab
shophy <77288022@qq.com>
2020-03-18 16:03:56 +0800

    添加解密数据和发送订阅消息

--- src/Api.php
+++ src/Api.php
@@ -3,18 +3,22 @@
 namespace shophy\miniprogram;
 
 use shophy\miniprogram\common\Utils;
+use shophy\miniprogram\exception\ApiException;
+use shophy\miniprogram\exception\InternalException;
+use shophy\miniprogram\exception\ParameterException;
 
 class Api
 {
-    const BASE_URL = 'https://api.weixin.qq.com';
-
-    const JSCODETOSESSION = '/sns/jscode2session?'; // 登录凭证校验
-    
-    public $errCode = 40001;
-    public $errMsg = "no access";
-    
+	const GET_ACCESSTOKEN = '/cgi-bin/token'; // 接口调用凭证
+	const JSCODETOSESSION = '/sns/jscode2session'; // 登录凭证校验
+	const MESSAGE_SUBSCRIBE_SEND = '/cgi-bin/message/subscribe/send?access_token=ACCESS_TOKEN'; // 发送订阅消息
+	
+	public $rspJson = null;
+	public $rspRawStr = null;
+	
     protected $appid = null;
-    protected $secret = null;
+	protected $secret = null;
+	protected $sessionKey = null;
     protected $accessToken = null;
 
 	public function __construct($appid, $secret)
@@ -24,105 +28,101 @@ class Api
 
         $this->appid = $appid;
         $this->secret = $secret;
-    }
-
-    public function getSessionByCode($code)
-    {
-        Utils::checkNotEmptyStr($code, 'code');
-        
-	    $result = $this->http_get(self::BASE_URL.self::JSCODETOSESSION.'appid='.$this->appid.'&secret='.$this->secret.'&js_code='.$code.'&grant_type=authorization_code');
-	    if ($result) {
-	        $json = json_decode($result, true);
-	        if (!$json)	return false;
-			if (isset($json['errcode']) && intval($json['errcode']) !== 0) {
-				$this->errCode = $json['errcode'];
-	            $this->errMsg = $json['errmsg'];
-	            return false;
-			}
-            
-	        return $json;
-        }
-        
-	    return false;
-    }
-    
-    /**
-	 * GET 请求
-	 * @param string $url
-	 */
-	private function http_get($url, &$http_info=null){
-		$oCurl = curl_init();
-		if(stripos($url,"https://")!==FALSE){
-			curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
-			curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
-			curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
-		}
-		curl_setopt($oCurl, CURLOPT_URL, $url);
-		curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
-		$sContent = curl_exec($oCurl);
-		$http_info = curl_getinfo($oCurl);
-		curl_close($oCurl);
-		if(intval($http_info["http_code"])==200){
-			return $sContent;
-		}else{
-			return false;
-		}
 	}
 
 	/**
-	 * POST 请求
-	 * @param string $url
-	 * @param array $param
-	 * @param boolean $post_file 是否文件上传
-	 * @return string content
+	 * 登录
 	 */
-	private function http_post($url,$param,$post_file=false){
-		$oCurl = curl_init();
-		if(stripos($url,"https://")!==FALSE){
-			curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
-			curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
-			curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
-		}
-	        if (PHP_VERSION_ID >= 50500 && class_exists('\CURLFile')) {
-	            	$is_curlFile = true;
-	        } else {
-	        	$is_curlFile = false;
-	            	if (defined('CURLOPT_SAFE_UPLOAD')) {
-	                	curl_setopt($oCurl, CURLOPT_SAFE_UPLOAD, false);
-	            	}
-	        }
-		if (is_string($param)) {
-	            	$strPOST = $param;
-	        }elseif($post_file) {
-	            	if($is_curlFile) {
-		                foreach ($param as $key => $val) {
-		                    	if (substr($val, 0, 1) == '@') {
-		                        	$param[$key] = new \CURLFile(realpath(substr($val,1)));
-		                    	}
-		                }
-	            	}
-			$strPOST = $param;
-		} else {
-			$aPOST = array();
-			foreach($param as $key=>$val){
-				$aPOST[] = $key."=".urlencode($val);
-			}
-			$strPOST =  join("&", $aPOST);
-		}
-		curl_setopt($oCurl, CURLOPT_URL, $url);
-		curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
-		curl_setopt($oCurl, CURLOPT_POST,true);
-		curl_setopt($oCurl, CURLOPT_POSTFIELDS,$strPOST);
-		$sContent = curl_exec($oCurl);
-		$aStatus = curl_getinfo($oCurl);
-		curl_close($oCurl);
-		if(intval($aStatus["http_code"])==200){
-			return $sContent;
-		}else{
-			return false;
-		}
+    public function getSessionByCode($code)
+    {
+		Utils::checkNotEmptyStr($code, 'code');
+		self::_HttpCall(self::JSCODETOSESSION, 'GET', array('appid' => $this->appid, 'secret' => $this->secret, 'js_code' => $code, 'grant_type' => 'authorization_code'));
 	}
 
+	/**
+     * 发送订阅消息
+     * @param $toUser 接收者（用户）的 openid
+     * @param $templateId 模板id
+     * @param $data
+     * @return throw exception
+     */
+	public function subscribeMessage($toUser, $templateId, $data, $page='')
+	{
+		$params = array(
+            'touser' => $toUser,
+            'template_id' => $templateId,
+			'data' => $data
+		);
+		empty($page) || $params['page'] = $page;
+		self::_HttpCall(self::MESSAGE_SUBSCRIBE_SEND, 'POST', $params);
+    }
+
+	/**
+     * @brief GetAccessToken : 获取 accesstoken，不用主动调用
+     *
+     * @return : string accessToken
+     */
+    public function GetAccessToken()
+    {
+        if ( ! Utils::notEmptyStr($this->accessToken)) { 
+            $this->RefreshAccessToken();
+        } 
+        return $this->accessToken;
+    }
+
+    protected function RefreshAccessToken($bflush=false)
+    {
+        if (!Utils::notEmptyStr($this->appid) || !Utils::notEmptyStr($this->secret))
+            throw new ParameterException("invalid appid or secret");
+
+        // 尝试从缓存读取,corpid 作为key
+        $this->accessToken = $bflush ? '' : $this->getCache($this->appid);
+        if( ! Utils::notEmptyStr($this->accessToken)) {
+			$url = self::BASE_URL.self::GET_ACCESSTOKEN."?grant_type=client_credential&appid={$this->appid}&secret={$this->secret}";
+            $this->_HttpGetParseToJson($url, false);
+            $this->_CheckErrCode();
+    
+            // 写入缓存
+			$this->accessToken = $this->rspJson["access_token"];
+			$this->setCache($this->appid, $this->accessToken, $this->rspJson['expires_in']);
+        }
+	}
+	
+	/**
+     * 检验数据的真实性，并且获取解密后的明文
+     * @param $encryptedData string 加密的用户数据
+     * @param $iv string 与用户数据一同返回的初始向量
+     * @param $data string 解密后的原文
+     *
+     * @return int 成功0，失败返回对应的错误信息
+     */
+    public function decryptData($encryptedData, $iv, &$data )
+    {
+		if (!Utils::notEmptyStr($this->sessionKey) || strlen($this->sessionKey) != 24) {
+			throw new ParameterException("invalid sessionKey");
+		}
+		if (!Utils::notEmptyStr($iv) || strlen($iv) != 24) {
+			throw new ParameterException("invalid iv");
+		}
+
+		$aesIV = base64_decode($iv);
+		$aesKey = base64_decode($this->sessionKey);
+		$aesCipher = base64_decode($encryptedData);
+
+		$result = openssl_decrypt($aesCipher, "AES-128-CBC", $aesKey, 1, $aesIV);
+		$dataObj = json_decode( $result );
+		if( $dataObj  == NULL )
+		{
+			throw new InternalException("decrypt failed");
+		}
+		if( $dataObj->watermark->appid != $this->appid )
+		{
+			throw new InternalException("invalid decrypt data");
+		}
+
+		$data = $result;
+    }
+	
 	/**
 	 * 设置缓存，按需重载
 	 * @param string $cachename
@@ -154,4 +154,132 @@ class Api
 		//TODO: remove cache implementation
 		//return Yii::$app->cache->delete($cachename);;
 	}
+    
+    protected function _HttpCall($url, $method, $args)
+    {
+        if ('POST' == $method) { 
+            $url = HttpUtils::MakeUrl($url);
+            $this->_HttpPostParseToJson($url, $args);
+            $this->_CheckErrCode();
+        } else if ('GET' == $method) { 
+            if (is_array($args) && count($args) > 0) { 
+                foreach ($args as $key => $value) {
+                    if ($value == null) continue;
+                    if (strpos($url, '?')) {
+                        $url .= ('&'.$key.'='.$value);
+                    } else { 
+                        $url .= ('?'.$key.'='.$value);
+                    }
+                }
+            }
+            $url = HttpUtils::MakeUrl($url);
+            $this->_HttpGetParseToJson($url);
+            $this->_CheckErrCode();
+        } else { 
+            throw new ApiException('wrong method');
+        }
+    }
+
+    protected function _HttpGetParseToJson($url, $refreshTokenWhenExpired=true)
+    {
+        $retryCnt = 0;
+        $this->rspJson = null;
+        $this->rspRawStr = null;
+
+        while ($retryCnt < 2) {
+            $tokenType = null;
+            $realUrl = $url;
+
+            if (strpos($url, "ACCESS_TOKEN")) {
+                $token = $this->GetAccessToken();
+                $realUrl = str_replace("ACCESS_TOKEN", $token, $url);
+                $tokenType = "ACCESS_TOKEN";
+            } else { 
+                $tokenType = "NO_TOKEN";
+            }
+
+            $this->rspRawStr = HttpUtils::httpGet($realUrl);
+
+            if ( ! Utils::notEmptyStr($this->rspRawStr)) throw new ApiException("empty response"); 
+			
+			$this->rspJson = json_decode($this->rspRawStr, true/*to array*/);
+            if (strpos($this->rspRawStr, "errcode") !== false) {
+                $errCode = Utils::arrayGet($this->rspJson, "errcode");
+                if ($errCode == 40014 || $errCode == 41001 || $errCode == 42001) { // token expired
+                    if ("NO_TOKEN" != $tokenType && true == $refreshTokenWhenExpired) {
+                        if ("ACCESS_TOKEN" == $tokenType) { 
+                            $this->RefreshAccessToken(true);
+                        }
+                        $retryCnt += 1;
+                        continue;
+                    }
+                }
+            }
+            return $this->rspRawStr;
+        }
+    }
+
+    protected function _HttpPostParseToJson($url, $args, $refreshTokenWhenExpired=true, $isPostFile=false)
+    {
+        $postData = $args;
+        if (!$isPostFile) {
+            if (!is_string($args)) {
+                $postData = HttpUtils::Array2Json($args);
+            }
+        }
+        $this->rspJson = null; $this->rspRawStr = null;
+
+        $retryCnt = 0;
+        while ($retryCnt < 2) {
+            $tokenType = null;
+            $realUrl = $url;
+
+    		if (strpos($url, "ACCESS_TOKEN")) {
+                $token = $this->GetAccessToken();
+                $realUrl = str_replace("ACCESS_TOKEN", $token, $url);
+                $tokenType = "ACCESS_TOKEN";
+            } else { 
+                $tokenType = "NO_TOKEN";
+            }
+
+            $this->rspRawStr = HttpUtils::httpPost($realUrl, $postData);
+
+            if ( ! Utils::notEmptyStr($this->rspRawStr)) throw new ApiException("empty response"); 
+
+            $json = json_decode($this->rspRawStr, true/*to array*/);
+            $this->rspJson = $json;
+
+            $errCode = Utils::arrayGet($this->rspJson, "errcode");
+            if ($errCode == 40014 || $errCode == 41001 || $errCode == 42001) { // token expired
+                if ("NO_TOKEN" != $tokenType && true == $refreshTokenWhenExpired) {
+                    if ("ACCESS_TOKEN" == $tokenType) { 
+                        $this->RefreshAccessToken(true);
+                    }
+                    $retryCnt += 1;
+                    continue;
+                }
+            }
+
+            return $json;
+        }
+    } 
+
+    protected function _CheckErrCode()
+    {
+        $rsp = $this->rspJson;
+        $raw = $this->rspRawStr;
+        if (is_null($rsp))
+            return;
+
+        if (!is_array($rsp))
+            throw new ParameterException("invalid type " . gettype($rsp));
+        if (!array_key_exists("errcode", $rsp)) {
+            return;
+        }
+        $errCode = $rsp["errcode"];
+        if (!is_int($errCode))
+            throw new ApiException("invalid errcode type " . gettype($errCode) . ":" . $raw);
+        if ($errCode != 0)
+            throw new ApiException("response error:" . $raw);
+    }
 }
